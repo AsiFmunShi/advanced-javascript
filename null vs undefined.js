1bbf9ba812d573471141158f1dca892ebdad0e02
Your Name <you@example.com>
2020-03-17 22:02:15 +0600

    blabla

---
+++ null vs undefined.js
@@ -0,0 +1,13 @@
+let name;
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    return;
+}
+
+const details = {name:"asif", id:28};
+console.log(details.roll);
+
+let fun = undefined;
+console.log(fun);
+
